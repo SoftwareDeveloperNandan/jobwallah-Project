34f5553fcfddd02db732f494b9e83993d71e85ec
SoftwareDeveloperNandan <nandanpanditsd@gmail.com>
2024-02-24 22:35:00 +0530

    Landing Page somthing change

---
+++ javascript/script.js
@@ -0,0 +1,8 @@
+
+let typed = new Typed('.js-typedjs', {
+    strings: ["Quiz", "Study-material", "Internship", "Job"],
+    typeSpeed: 90,
+    loop: true,
+    backSpeed: 30,
+    backDelay: 2500
+})
